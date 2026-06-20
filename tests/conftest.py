65371d15860e2cde5218b10a0f44575a1f658d74
Guilhem Allaman <contact@guilhemallaman.net>
2026-06-20 15:32:24 +0200

    test: add typing and button `get_item` check

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
+from collections.abc import Generator
+from pytest_qfield.qfieldbot import QFieldBot
+
 import pytest
 
 from tests import PLUGIN_QML_PATH
 
 
 @pytest.fixture
-def loaded_plugin(qfield_bot):
-    qfield_bot.load_plugin(PLUGIN_QML_PATH)
+def qfield_bot_loaded(qfield_bot: QFieldBot) -> Generator[QFieldBot, None, None]:
+    # TODO: investigate and remove `raise_if_warnings=False`
+    qfield_bot.load_plugin(PLUGIN_QML_PATH, raise_if_warnings=False)
     yield qfield_bot
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,11 +1,20 @@
-# from tests import PLUGIN_QML_PATH
+from pytest_qfield.qfieldbot import QFieldBot
+from pytest_qfield.stub_interface.qfield_stubs import QFieldAppInterfaceStub
+
+from tests import PLUGIN_QML_PATH
 
 
-def test_qfield_bot_is_there(qfield_bot):
+def test_qfield_bot_is_there(qfield_bot: QFieldBot):
     assert qfield_bot is not None
 
-def test_qfield_iface_is_there(qfield_iface):
+def test_qfield_iface_is_there(qfield_iface: QFieldAppInterfaceStub):
     assert qfield_iface is not None
 
-# def test_plugin_loads_with_errours(qfield_bot):
-#     qfield_bot.load_plugin(PLUGIN_QML_PATH)
+def test_plugin_loads_without_errors(qfield_bot: QFieldBot):
+    # TODO: investigate and remove `raise_if_warnings=False`
+    qfield_bot.load_plugin(PLUGIN_QML_PATH, raise_if_warnings=False)
+
+def test_buttons_available(qfield_bot_loaded: QFieldBot):
+    assert qfield_bot_loaded.get_item("pluginButton") is not None
+    assert qfield_bot_loaded.get_item("pluginQChatButton") is not None
+    assert qfield_bot_loaded.get_item("pluginNewsButton") is not None
