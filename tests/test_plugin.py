fa125d1de467209214c8188e687ad5bbe590dab2
Guilhem Allaman <contact@guilhemallaman.net>
2026-06-20 09:18:42 +0200

    test: edit basic test file

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,9 +1,7 @@
-from pytest_qfield.qfieldbot import QFieldBot
-
-def test_qfield_bot_is_there(qfield_bot: QFieldBot):
+def test_qfield_bot_is_there(qfield_bot):
     """Test that the QField bot is available."""
     assert qfield_bot is not None
 
-def test_qfield_new_project_is_there(qfield_new_project: "QFieldProject"):
-    """Test that the new QField project is available."""
-    assert qfield_new_project is not None
+def test_qfield_iface_is_there(qfield_iface):
+    """Test that the QField iface is available."""
+    assert qfield_iface is not None
